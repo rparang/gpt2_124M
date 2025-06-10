e7ceac4367ce33a26522b27e56d9b5a53fa1dfff
Reza <reza@Samiras-MacBook-Air.local>
2025-06-10 14:07:40 -0700

    Implemented CausalSelfAttention class

--- gpt_124.py
+++ gpt_124.py
@@ -5,8 +5,61 @@ from torch.nn import functional as F
 
 # ----------------------------------------------------------
 
+class CausalSelfAttention(nn.Module):
+
+	def __init__(self, config):
+		super().__init__()
+		assert config.n_embd % config.n_head == 0
+
+		self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd) # key, query, value projections for all heads but in a batch. Saves you from three separate instantiations of nn.Linear
+		self.proj = nn.Linear(config.n_embd, config.n_embd) # output projection
+
+		self.n_head = config.n_head
+		self.n_embd = config.n_embd
+
+		self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size)).view(1, 1, config.block_size, config.block_size))
+
+	def forward(self, x):
+
+		B, T, C = x.size() # batch size, sequence length, embedding dimension (n_embd)
+
+		# Calculate query, key, value for all heads in batch, move head forward in the shape to be a batch dim alongside B
+		# nh is "number of heads", hs is "head size", and C is number of channels (nh * hs)
+		# e.g. in GPT-2 (124M), n_head=12, hs=64, so nh*hs = 768 channels in the Transformer
+
+		qkv = self.c_attn(x)
+		q, k, v = qkv.split(self.n_embd, dim=2)
+		k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+		q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+		v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+
+		# attention materializes the large (T, T) matrix for all queries and keys
+		att = q @ k.transpose(-2, -1) * (1.0 / math.sqrt(k.size(-1))) # --> (B, nh, T, T)
+		att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf'))
+		att = F.softmax(att, dim=-1)
+
+		y = att @ v # (B, nh, T, T) x (B, nh, T, hs) --> (B, nh, T, hs)
+		y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+
+		# output project
+		y = self.proj(y)
+		return y
 
 
+
+class MLP(nn.Module):
+
+	def __init__(self, config):
+		super().__init__()
+		self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd) # On naming (eg 'c_fc'), we are replicating the GPT2 model
+		self.gelu = nn.GELU(approximate='tanh')
+		self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+
+	def forward(self, x):
+		x = self.c_fc(x)
+		x = self.gelu(x)
+		x = self.c_proj(x)
+		return x
 
 
 class Block(nn.Module):
