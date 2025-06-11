991afeb7b14d7c458e6841c89de8422f1d1afcba
Reza <reza@Samiras-MacBook-Air.local>
2025-06-11 11:36:06 -0700

    implemented from_pretrained method to copy the GPT2 model params from huggingface to model built from scratch

--- gpt_124.py
+++ gpt_124.py
@@ -46,7 +46,6 @@ class CausalSelfAttention(nn.Module):
 		return y
 
 
-
 class MLP(nn.Module):
 
 	def __init__(self, config):
@@ -80,22 +79,87 @@ class Block(nn.Module):
 
 @dataclass
 class GPTConfig:
-	block_size: int = 256
-	vocab_size: int = 65
-	n_layer: int = 6
-	n_head: int = 6
-	n_embd: int = 384
+	block_size: int = 1024 # max sequence length
+	vocab_size: int = 50257 # number of tokens: 50,000 BPE merges + 256 bytes tokens + 1 <|endoftext|> token
+	n_layer: int = 12 # number of layers
+	n_head: int = 12 # number of heads
+	n_embd: int = 768 # embedding dimension
 
 class GPT(nn.Module):
-    
-    def __init__(self, config):
-        super().__init__()
-        self.config = config
+	
+	def __init__(self, config):
+		super().__init__()
+		self.config = config
 
-        self.transformer = nn.ModuleDict(dict(
-            wte = nn.Embedding(config.vocab_size, config.n_embd),
-            wpe = nn.Embedding(config.block_size, config.n_embd),
-            h = nn.ModuleList([Block(config) for _ in range(config.n_layers)]),
-            ln_f = nn.LayerNorm(config.n_embd)
-        ))
-        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+		self.transformer = nn.ModuleDict(dict(
+			wte = nn.Embedding(config.vocab_size, config.n_embd),
+			wpe = nn.Embedding(config.block_size, config.n_embd),
+			h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+			ln_f = nn.LayerNorm(config.n_embd)
+		))
+		self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+	@classmethod
+	def from_pretrained(cls, model_type):
+		"""Loads pretrained GPT-2 model weights from huggingface"""
+		assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
+		from transformers import GPT2LMHeadModel
+		print("loading weights from pretrained gpt: %s" % model_type)
+
+		# n_layer, n_head and n_embd are determined from model_type
+		config_args = {
+			'gpt2':			dict(n_layer=12, n_head=12, n_embd=768), 	# 124M params
+			'gpt2-medium':	dict(n_layer=24, n_head=16, n_embd=1024), 	# 350M params
+			'gpt2-large':	dict(n_layer=36, n_head=20, n_embd=1280), 	# 774M param
+			'gpt2-xl':		dict(n_layer=48, n_head=25, n_embd=1600), 	# 1558M params
+		}[model_type]
+		config_args['vocab_size'] = 50257 # always 50257 for GPT model checkpoints
+		config_args['block_size'] = 1024  # always 1024 for GPT model checkpoints
+
+		# create a from-scratch initialized minGPT model
+		config = GPTConfig(**config_args)
+		model = GPT(config)
+		sd = model.state_dict()
+		sd_keys = sd.keys()
+		sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')] # dicard the mask / buffer
+
+		# init a huggingface/transformers model
+		model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+		sd_hf = model_hf.state_dict()
+
+		# copy while ensuring all of the parameters are aligned and match in names and shapes
+		sd_keys_hf = sd_hf.keys()
+		sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')] # dicard the mask / buffer
+		sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')] # dicard the mask / buffer
+		transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+		# basically the openai checkppoints use a "Conv1D" module, but we only want to use a vanilla Linear
+		# this means we have to transpose these weights when we import them
+		assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+		for k in sd_keys_hf:
+			if any(k.endswith(w) for w in transposed):
+				# special treatment for the Conv1D weights we need to transpose
+				assert sd_hf[k].shape[::-1] == sd[k].shape
+				with torch.no_grad():
+					sd[k].copy_(sd_hf[k].t())
+			else:
+				assert sd_hf[k].shape == sd[k].shape
+				with torch.no_grad():
+					sd[k].copy_(sd_hf[k])
+
+		return model
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
