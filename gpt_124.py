d00b6185298818fb509a56c9cf7dc4bb5b598c39
Reza <reza.parang@gmail.com>
2025-06-18 13:17:08 -0700

    learning rate scheduler

--- gpt_124.py
+++ gpt_124.py
@@ -239,7 +239,7 @@ if torch.cuda.is_available():
 	torch.cuda.manual_seed(1337)
 
 
-train_loader = DataLoaderLite(B=4, T=32)
+train_loader = DataLoaderLite(B=8, T=64)
 
 torch.set_float32_matmul_precision('high')
 
@@ -248,12 +248,31 @@ model = GPT(GPTConfig())
 model.to(device)
 # model = torch.compile(model)
 
+
+max_lr = 6e-4
+min_lr = max_lr * 0.1
+warmup_steps = 10
+max_steps = 50
+def get_lr(it):
+	# 1) linear warmup for warmup_iters steps
+	if it < warmup_steps:
+		return max_lr * (it+1) / warmup_steps
+	# 2) if it > lr_decay_iters, return min learning rate
+	if it > max_steps:
+		return min_lr
+	# 3) in between, use cosine decay down to min learning rate
+	decay_ratio = (it - warmup_steps) / (max_steps - warmup_steps)
+	assert 0 <= decay_ratio <= 1
+	coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio)) # coeff starts at 1 and goes to 0
+	return min_lr + coeff * (max_lr - min_lr)
+
+
 # Number of parameters
-print(sum([p.nelement() for p in model.parameters()]))
+print("Number of parameters:", sum([p.nelement() for p in model.parameters()]))
 
 # optimize!
-optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
-for i in range(50):
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-8)
+for step in range(max_steps):
 	t0 = time.time()
 	x, y = train_loader.next_batch()
 	x, y = x.to(device), y.to(device)
@@ -262,11 +281,17 @@ for i in range(50):
 		logits, loss = model(x, y)
 		# import code; code.interact(local=locals())
 	loss.backward()
+	norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+	# determine and set the learning rate for this iteration
+	lr = get_lr(step)
+	for param_group in optimizer.param_groups:
+		param_group['lr'] = lr
 	optimizer.step()
+	# torch.cuda.synchronize() # wait for the GPU to finish work
 	t1 = time.time()
 	dt = (t1 - t0) * 1000 # time diff in milliseconds
 	tokens_per_sec = (train_loader.B * train_loader.T) / (t1 - t0)
-	print(f"step {i}, loss: {loss.item()}, dt: {dt:.2f}ms, tok/sec: {tokens_per_sec}")
+	print(f"step: {step} | loss: {loss.item():.6f} | lr: {lr:.4e} | norm: {norm:.4f} | dt: {dt:.2f}ms | tok/sec: {tokens_per_sec}")
 
 print(loss)
 # print(logits.shape)
